9096fdde40a0048cf2eecf832b75b5fa868f8556
Mia Pickel <Mia.Pickel@nordicsemi.no>
2025-06-24 13:05:01 +0200

    fix: re-add commented out code (forgot)

--- src/jlink.ts
+++ src/jlink.ts
@@ -54,19 +54,18 @@ interface Update {
     percentage: number;
 }
 
-const downloadJLink = async ({ jlinkUrl: jlinkUrls }: JLinkIndex, onUpdate?: (update: Update) => void): Promise<string> => {
-    // const platform = os.platform();
-    // const arch = os.arch();
-    // // // @ts-expect-error It is quite literally checked right before
-    // if (!(platform in jlinkUrls) || !(arch in jlinkUrls[platform])) {
-    //     throw new Error(`JLink not available for ${platform}/${arch}`)
-    // }
-    // // // @ts-expect-error We know it exists but it is also handled if undefined
-    // const url = jlinkUrls[platform]?.[arch]
-    // if (!url) {
-    //     throw new Error(`JLink not available for ${platform}/${arch}`)
-    // }
-    const url =""
+const downloadJLink = async ({ jlinkUrls }: JLinkIndex, onUpdate?: (update: Update) => void): Promise<string> => {
+    const platform = os.platform();
+    const arch = os.arch();
+    // @ts-expect-error It is quite literally checked right before
+    if (!(platform in jlinkUrls) || !(arch in jlinkUrls[platform])) {
+        throw new Error(`JLink not available for ${platform}/${arch}`)
+    }
+    // @ts-expect-error We know it exists but it is also handled if undefined
+    const url = jlinkUrls[platform]?.[arch]
+    if (!url) {
+        throw new Error(`JLink not available for ${platform}/${arch}`)
+    }
     const {
         status,
         data: stream,
